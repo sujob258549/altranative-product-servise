4a5a1183d104a3b4c4391483766ba49c7a4ffa06
DevSujon <sujonmaia274@gmail.com>
2024-05-12 16:35:02 +0600

    delet

--- index.js
+++ index.js
@@ -79,6 +79,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await mainProductColection.deleteOne(query);
+            res.send(result)
+
+        })
 
 
         // Connect the client to the server	(optional starting in v4.7)
