f5526d2a3bdff7ffd4d5253da728da8ec4d41d63
DevSujon <sujonmaia274@gmail.com>
2024-05-12 08:09:44 +0600

    git ignore

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const port =process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true
+}))
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PAS}@atlascluster.aasa6jh.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Defrent product!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
